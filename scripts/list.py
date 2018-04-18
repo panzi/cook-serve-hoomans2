eab571be48c9d490af0e468aee990e6356b89817
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2018-04-18 03:40:43 +0200

    added nebbishHeretic

---
+++ scripts/list.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import csv
+from os.path import join as pjoin, abspath, dirname
+
+def make_list():
+    names = []
+    with open(pjoin(dirname(abspath(__file__)), "..", "hoomans.csv")) as fp:
+        for line in sorted(list(row[0].strip() for row in csv.reader(fp)), key=lambda name: name.lower()):
+            names.append(line[:25].strip())
+    n = len(names)
+    part_count = 4
+    partlen = (n + part_count - 1) // part_count
+    lines = []
+    maxlens = [0] * part_count
+    for off in range(partlen):
+        line = []
+        for part in range(part_count):
+            i = part * partlen + off
+            if i < n:
+                name = names[i]
+                namelen = len(name)
+                if namelen > maxlens[part]:
+                    maxlens[part] = namelen
+                line.append(name)
+        lines.append(line)
+    
+    for line in lines:
+        outline = []
+        for maxlen, name in zip(maxlens, line):
+            pad = maxlen - len(name)
+            outline.append(name + (' ' *pad))
+        print('   '.join(outline).rstrip())
+
+if __name__ == '__main__':
+    make_list()
