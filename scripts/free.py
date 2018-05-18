d526c37275d0d8f55bc4ebdcbb0175396e09baf6
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2018-05-18 22:46:16 +0200

    correct free.py

--- scripts/free.py
+++ scripts/free.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 
-import csv
+import os
 from os.path import join as pjoin, abspath, dirname
 
 FOLDERS = ('CUST_SPR_POOR', 'CUST_SPR_COMMON', 'CUST_SPR_RICH')
 
 def free():
 	free_map = {folder: set(range(73)) for folder in FOLDERS}
+	prjdir = pjoin(dirname(abspath(__file__)), '..')
 
-	with open(pjoin(dirname(abspath(__file__)), '..', 'hoomans.csv'), 'r') as fp:
-		for row in csv.reader(fp):
-			hpath = row[1].strip()
-			folder, filename = hpath.split("/")
-			number, ext = filename.split(".")
-			if ext == "png":
-				free_map[folder].remove(int(number))
+	for folder in FOLDERS:
+		spritedir = pjoin(prjdir, 'sprites', folder)
+		free_set = free_map[folder]
+		for filename in os.listdir(spritedir):
+			parts = filename.split(".")
+			if len(parts) == 2 and parts[1] == "png":
+				try:
+					number = int(parts[0], 10)
+				except ValueError:
+					pass
+				else:
+					free_set.remove(number)
 
 	for folder in FOLDERS:
 		free_slots = free_map[folder]
 		if free_slots:
-			print(folder+ ':')
+			print(folder + ':')
 			for number in sorted(free_slots):
 				print('\t%s/%d.png' % (folder, number))
 		print()
